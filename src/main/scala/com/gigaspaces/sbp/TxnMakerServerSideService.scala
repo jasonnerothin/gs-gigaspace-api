e99ff075ea6a4e7e1c00987370b871188f97deec
Jason Nerothin <jason.nerothin@kohls.com>
2014-05-26 16:24:53 -0500

    ignoring gslicense.xml

--- src/main/scala/com/gigaspaces/sbp/TxnMakerServerSideService.scala
+++ src/main/scala/com/gigaspaces/sbp/TxnMakerServerSideService.scala
@@ -8,6 +8,7 @@ import javax.annotation.Resource
 import org.openspaces.core.GigaSpace
 import scala.concurrent._
 import ExecutionContext.Implicits.global
+import org.slf4j.{LoggerFactory, Logger}
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,6 +18,8 @@ import ExecutionContext.Implicits.global
  */
 @RemotingService
 class TxnMakerServerSideService extends TransactionMaker {
+
+  val logger:Logger = LoggerFactory.getLogger(getClass)
 
   @Resource
   var gigaSpace: GigaSpace = null
@@ -28,6 +31,8 @@ class TxnMakerServerSideService extends TransactionMaker {
     */
   @Transactional
   def longTransaction(routeId: Integer, spaceId: java.lang.String, howLongInMillis: Integer = 50): Unit = {
+
+    logger.trace("long transaction received...")
 
     val idQuery = new IdQuery[SpaceThing](classOf[SpaceThing], spaceId, routeId)
     val changeSet = new ChangeSet().set("payload", "laDeeDah")
